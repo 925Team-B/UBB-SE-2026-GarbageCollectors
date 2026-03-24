f8870b73306e8ce51719e96beec2de393d8c6e7d
taisia512 <mocanutaisia@gmail.com>
2026-03-24 19:20:57 +0200

    ticket_68
    RecurringPayment

--- BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Views/RecurringPaymentsPage.xaml.cs
+++ BankingAppTeamB/BankingAppTeamB/BankingAppTeamB/Views/RecurringPaymentsPage.xaml.cs
@@ -1,31 +1,60 @@
-using Microsoft.UI.Xaml;
+using BankingAppTeamB.Configuration;
+using BankingAppTeamB.ViewModels;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BankingAppTeamB.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class RecurringPaymentsPage : Page
     {
+        private readonly RecurringPaymentViewModel _viewModel;
+
         public RecurringPaymentsPage()
         {
             InitializeComponent();
+            _viewModel = new RecurringPaymentViewModel(ServiceLocator.RecurringPaymentService);
+            DataContext = _viewModel;
+
+            StartDatePicker.Date = DateTimeOffset.Now;
+            EndDatePicker.Date = DateTimeOffset.Now;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await _viewModel.LoadAsync();
+        }
+
+        private void AmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (DataContext is RecurringPaymentViewModel vm && sender is TextBox textBox)
+            {
+                if (decimal.TryParse(textBox.Text, out decimal value))
+                {
+                    vm.Amount = value;
+                }
+                else
+                {
+                    vm.Amount = 0;
+                }
+            }
+        }
+
+        private void StartDatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs args)
+        {
+            if (DataContext is RecurringPaymentViewModel vm && sender is DatePicker picker)
+            {
+                vm.StartDate = picker.Date.DateTime.Date;
+            }
+        }
+
+        private void EndDatePicker_DateChanged(object sender, DatePickerValueChangedEventArgs args)
+        {
+            if (DataContext is RecurringPaymentViewModel vm && sender is DatePicker picker)
+            {
+                vm.EndDate = picker.Date.DateTime.Date;
+            }
         }
     }
 }
